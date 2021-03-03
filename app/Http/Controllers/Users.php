295eabb8113e37754a0b938b8773f4e0d35d8930
12345adnan <12345adnan@gmail.com>
2021-03-03 10:11:27 +0500

    api snactam

---
+++ app/Http/Controllers/Users.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Facades\Auth;
+
+class Users extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+
+        $idinstance = User::Create(
+            [
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+                'device' => $request->device,
+
+            ]
+        );
+
+        $user = User::where("id", $idinstance->id)->first();
+
+        $tokenResult = $user->createToken($request->device)->plainTextToken;
+        return response()->json(
+            [
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+            ]);
+    }
+
+
+    public function logins(Request $request)
+    {
+
+        try {
+            $request->validate([
+                    'email' => 'email|required',
+                    'password' => 'required']
+            );
+            $credentials = request([
+                'email', 'password'
+            ]);
+            if (!Auth::attempt($credentials)) {
+                return response()->json(
+                    [
+                        'status_code' => 500,
+                        'message' => 'Unauthorized'
+                    ]);
+            }
+            $user = User::where('email', $request->email)->first();
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error in Login');
+            }
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return response()->json(
+                [
+                    'status_code' => 200,
+                    'access_token' => $tokenResult,
+                    'token_type' => 'Bearer',]
+            );
+        } catch (Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Login',
+                'error' => $error,
+            ]);
+        }
+
+    }
+
+    public function all(Request $request)
+    {
+        $user = User::all();
+
+        return response()->json(
+            [
+                'status_code' => 401,
+                'login' => "faliure",
+                'user' => $user,
+
+            ]);
+    }
+
+}
---
+++ routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\Users;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('/register', [Users::class, 'store']);
+
+
+Route::post('/logins', [Users::class, 'logins']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/users', [Users::class, 'all']);
+
+});
